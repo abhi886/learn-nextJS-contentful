b8845e618b6b156cc3107ee5d01ae9b25dfb94f9
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2021-08-29 12:02:36 +1000

    Incremental Static Regeneration

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export async function getStaticProps() {
   return {
     props: {
       receipes: res.items,
+      revalidate: 1,
     },
   };
 }
--- pages/receipes/[slug].js
+++ pages/receipes/[slug].js
@@ -32,6 +32,7 @@ export async function getStaticProps({ params }) {
   return {
     props: {
       receipes: items[0],
+      revalidate: 1,
     },
   };
 }
